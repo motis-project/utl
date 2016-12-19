461c1765a5383367e8d8e2051e95157627be3e0b
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2016-12-19 20:23:13 +0100

    append -> concat

--- include/common/concat.h
+++ include/common/concat.h
@@ -5,7 +5,7 @@
 namespace common {
 
 template <typename Container>
-void append(Container& c1, Container const& c2) {
+void concat(Container& c1, Container const& c2) {
   c1.insert(end(c1), begin(c2), end(c2));
 }
 
