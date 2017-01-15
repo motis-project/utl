61ec2a1f59f3e751aa875fe37a7c4ca8078e663f
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2017-01-15 16:18:30 +0100

    erase_duplicates: overload with less and equals lambdas

--- include/utl/erase_duplicates.h
+++ include/utl/erase_duplicates.h
@@ -10,4 +10,10 @@ void erase_duplicates(Container& c) {
   c.erase(std::unique(begin(c), end(c)), end(c));
 }
 
+template <typename Container, typename Less, typename Eq>
+void erase_duplicates(Container& c, Less&& less, Eq&& eq) {
+  std::sort(begin(c), end(c), less);
+  c.erase(std::unique(begin(c), end(c), eq), end(c));
+}
+
 }  // namespace utl
