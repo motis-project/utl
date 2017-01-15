f26bfac085b88f5ffc8217e6e6ebbe4e362de654
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2017-01-15 16:18:40 +0100

    equal_ranges: initial version

---
+++ include/utl/equal_ranges.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+
+namespace utl {
+
+template <typename Container, typename Cmp, typename F>
+void equal_ranges(Container& c, Cmp&& cmp, F&& func) {
+  std::sort(begin(c), end(c), cmp);
+  auto lower = begin(c);
+  while (lower != end(c)) {
+    auto upper = std::upper_bound(begin(c), end(c), *lower, cmp);
+    func(lower, upper);
+    lower = upper;
+  }
+}
+
+}  // namespace utl
