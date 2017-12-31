5e178f0a530ae318bdc071c1fe6dcba623f4d586
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2017-12-31 17:07:39 +0100

    try fix MSVC

--- include/utl/struct/for_each_member.h
+++ include/utl/struct/for_each_member.h
@@ -6,16 +6,32 @@
 
 namespace utl {
 
+template <size_t Index, typename Fn, typename... Args>
+void call_for_each(Fn&& fn, std::tuple<Args...>&& t) {
+  fn(std::get<Index>(t));
+  if constexpr (Index != std::tuple_size_v<std::tuple<Args...>> - 1) {
+    call_for_each<Index + 1>(std::forward<Fn>(fn),
+                             std::forward<std::tuple<Args...>>(t));
+  }
+}
+
+template <typename Fn, typename... Args>
+void call_for_each(Fn&& fn, std::tuple<Args...>&& t) {
+  call_for_each<0, Fn, Args...>(std::forward<Fn>(fn),
+                                std::forward<std::tuple<Args...>>(t));
+}
+
 template <typename T, typename Fn>
-inline void for_each_field(T& t, Fn&& f) {
+inline void for_each_field(T& t, Fn&& fn) {
   if constexpr (std::is_pointer_v<T>) {
     if (t != nullptr) {
-      for_each_field(*t, std::forward<Fn>(f));
+      for_each_field(*t, std::forward<Fn>(fn));
     }
   } else if constexpr (std::is_scalar_v<T>) {
     f(t);
   } else {
-    std::apply([&](auto&&... args) { (f(args), ...); }, to_tuple(t));
+    call_for_each(std::forward<Fn>(fn), to_tuple(t));
+    //    std::apply([&](auto&&... args) { (f(args), ...); }, to_tuple(t));
   }
 }
 
