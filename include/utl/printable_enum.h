90d1f3b89010f3a8138df80e9290d5b961371ba8
Felix Gündling <felix.guendling@gmail.com>
2018-01-03 22:07:30 +0100

    printable enum

---
+++ include/utl/printable_enum.h
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#define PRINTABLE_ENUM(name, ...)                                    \
+  enum class name { __VA_ARGS__, __COUNT };                          \
+  inline std::string as_string(name const v) {                       \
+    std::string s = #__VA_ARGS__;                                    \
+    auto c = begin(s);                                               \
+    auto start = begin(s);                                           \
+    for (auto i = 0u; i <= static_cast<unsigned>(v); ++i) {          \
+      while (c != end(s) && (*c == ' ' || *c == ',')) {              \
+        ++c;                                                         \
+      }                                                              \
+      start = c;                                                     \
+      c = std::find(std::next(c), end(s), ',');                      \
+    }                                                                \
+    return {start, c};                                               \
+  }                                                                  \
+  inline std::ostream& operator<<(std::ostream& out, name const v) { \
+    return out << as_string(v);                                      \
+  }
---
+++ test/printable_enum.cc
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+
+#include "utl/printable_enum.h"
+
+PRINTABLE_ENUM(color, RED, GREEN, BLUE);
+
+TEST_CASE("printable_enum") {
+  CHECK(as_string(color::RED) == "RED");
+  CHECK(as_string(color::GREEN) == "GREEN");
+  CHECK(as_string(color::BLUE) == "BLUE");
+
+  std::stringstream ss;
+  ss << color::RED << color::GREEN << color::BLUE;
+  CHECK(ss.str() == "REDGREENBLUE");
+}
