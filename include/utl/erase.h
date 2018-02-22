23b29cff7bed548390ed9cbd8ba050c375d7c68b
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-02-22 15:37:37 +0100

    erase for set

--- include/utl/erase.h
+++ include/utl/erase.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include <algorithm>
+#include <set>
 
 namespace utl {
 
 template <typename Container, typename Element>
 void erase(Container& c, Element elem) {
   c.erase(std::remove(begin(c), end(c), elem), end(c));
+}
+
+template <typename T, typename Element>
+void erase(std::set<T>& c, Element elem) {
+  c.erase(elem);
 }
 
 }  // namespace utl
