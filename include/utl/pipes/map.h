64d8d5296bca9f1c0490754434e9bba1ae8c4385
Felix Gündling <felix.guendling@gmail.com>
2018-05-22 22:40:49 +0200

    pipes: map operations

---
+++ include/utl/pipes/map.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+
+namespace utl {
+
+struct to_map_t {
+  template <typename Range>
+  friend auto operator|(Range&& r, to_map_t&&) {
+    auto it = r.begin();
+    using value_t = decltype(r.read(it));
+    std::map<value_t> v;
+    while (r.valid(it)) {
+      auto entry = r.read(it);
+      v.emplace(std::move(entry.first), std::move(entry.second));
+      r.next(it);
+    }
+    return v;
+  }
+};
+
+inline to_map_t map() { return to_map_t(); }
+
+}  // namespace utl
