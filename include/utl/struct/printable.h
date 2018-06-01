1156032a502801303efd2d73b8fd8e892d28cd11
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-06-01 12:45:52 +0200

    raii util (utl_finally), ostream << vector<T>, interface library

--- include/utl/struct/printable.h
+++ include/utl/struct/printable.h
@@ -4,6 +4,22 @@
 
 #include "utl/struct/for_each_field.h"
 
+#ifndef UTL_PRINTABLE_NO_VEC
+template <typename T>
+inline std::ostream& operator<<(std::ostream& out, std::vector<T> const& v) {
+  out << "[\n  ";
+  auto first = true;
+  for (auto const& e : v) {
+    if (!first) {
+      out << ",\n  ";
+    }
+    out << e;
+    first = false;
+  }
+  return out << "\n]";
+}
+#endif
+
 #define MAKE_PRINTABLE(class_name)                                          \
   friend std::ostream& operator<<(std::ostream& out, class_name const& o) { \
     bool first = true;                                                      \
