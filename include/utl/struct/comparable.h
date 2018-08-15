fa90e5d25917f0c33c2a635f81aadc4dc03c3013
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-08-15 10:48:19 +0200

    MAKE_COMPARABLE operators const

--- include/utl/struct/comparable.h
+++ include/utl/struct/comparable.h
@@ -4,42 +4,42 @@
 
 #define MAKE_COMPARABLE()                             \
   template <typename T>                               \
-  bool operator==(T&& b) {                            \
+  bool operator==(T&& b) const {                      \
     static_assert(std::is_same_v<decltype(*this), T>, \
                   "comparison: types must match");    \
     return utl::to_tuple(*this) == utl::to_tuple(b);  \
   }                                                   \
                                                       \
   template <typename T>                               \
-  bool operator!=(T&& b) {                            \
+  bool operator!=(T&& b) const {                      \
     static_assert(std::is_same_v<decltype(*this), T>, \
                   "comparison: types must match");    \
     return utl::to_tuple(*this) != utl::to_tuple(b);  \
   }                                                   \
                                                       \
   template <typename T>                               \
-  bool operator<(T&& b) {                             \
+  bool operator<(T&& b) const {                       \
     static_assert(std::is_same_v<decltype(*this), T>, \
                   "comparison: types must match");    \
     return utl::to_tuple(*this) < utl::to_tuple(b);   \
   }                                                   \
                                                       \
   template <typename T>                               \
-  bool operator<=(T&& b) {                            \
+  bool operator<=(T&& b) const {                      \
     static_assert(std::is_same_v<decltype(*this), T>, \
                   "comparison: types must match");    \
     return utl::to_tuple(*this) <= utl::to_tuple(b);  \
   }                                                   \
                                                       \
   template <typename T>                               \
-  bool operator>(T&& b) {                             \
+  bool operator>(T&& b) const {                       \
     static_assert(std::is_same_v<decltype(*this), T>, \
                   "comparison: types must match");    \
     return utl::to_tuple(*this) > utl::to_tuple(b);   \
   }                                                   \
                                                       \
   template <typename T>                               \
-  bool operator>=(T&& b) {                            \
+  bool operator>=(T&& b) const {                      \
     static_assert(std::is_same_v<decltype(*this), T>, \
                   "comparison: types must match");    \
     return utl::to_tuple(*this) >= utl::to_tuple(b);  \
