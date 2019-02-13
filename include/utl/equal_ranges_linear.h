4831fcfa9a28534323f828fd0a07a414176f101e
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2019-02-13 10:15:45 +0100

    equal_ranges_linear: initial commit

---
+++ include/utl/equal_ranges_linear.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <algorithm>
+#include <iterator>
+
+namespace utl {
+
+template <typename Iterator, typename F>
+void equal_ranges_linear(Iterator begin, Iterator end, F&& func) {
+  auto lower = begin;
+  while (lower != end) {
+    auto upper = lower;
+    while (upper != end && *lower == *upper) {
+      ++upper;
+    }
+    func(lower, upper);
+    lower = upper;
+  }
+}
+
+template <typename Container, typename F>
+void equal_ranges_linear(Container& c, F&& func) {
+  equal_ranges_linear(std::begin(c), std::end(c), std::forward<F&&>(func));
+}
+
+template <typename Iterator, typename Eq, typename F>
+void equal_ranges_linear(Iterator begin, Iterator end, Eq&& eq, F&& func) {
+  auto lower = begin;
+  while (lower != end) {
+    auto upper = lower;
+    while (upper != end && eq(*lower, *upper)) {
+      ++upper;
+    }
+    func(lower, upper);
+    lower = upper;
+  }
+}
+
+template <typename Container, typename Eq, typename F>
+void equal_ranges_linear(Container& c, Eq&& eq, F&& func) {
+  equal_ranges_linear(std::begin(c), std::end(c), std::forward<Eq&&>(eq),
+                      std::forward<F&&>(func));
+}
+
+}  // namespace utl
