08b234b36ee711cfc7bc9946b37afc353daab956
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2019-02-13 10:15:12 +0100

    parallel_for: more variants (index, less verbose)

--- include/utl/parallel_for.h
+++ include/utl/parallel_for.h
@@ -9,30 +9,40 @@
 
 namespace utl {
 
-template <typename T, typename Fun>
-inline void parallel_for(std::string const& desc, std::vector<T> const& jobs,
-                         size_t const mod, Fun func) {
+template <typename Fun>
+inline void parallel_for(size_t const job_count, Fun func) {
   std::atomic<size_t> counter(0);
   std::vector<std::thread> threads;
   for (auto i = 0u; i < std::thread::hardware_concurrency(); ++i) {
-    // for (auto i = 0u; i < 1; ++i) {
     threads.emplace_back([&]() {
       while (true) {
         auto const idx = counter.fetch_add(1);
-        if (idx >= jobs.size()) {
+        if (idx >= job_count) {
           break;
         }
 
-        if (idx % mod == 0) {
-          uLOG(info) << desc << " " << idx << "/" << jobs.size();
-        }
-
-        func(jobs[idx]);
+        func(idx);
       }
     });
   }
 
   std::for_each(begin(threads), end(threads), [](auto& t) { t.join(); });
+}
+
+template <typename T, typename Fun>
+inline void parallel_for(std::vector<T> const& jobs, Fun func) {
+  parallel_for(jobs.size(), [&](auto const idx) { func(jobs[idx]); });
+}
+
+template <typename T, typename Fun>
+inline void parallel_for(std::string const& desc, std::vector<T> const& jobs,
+                         size_t const mod, Fun func) {
+  parallel_for(jobs.size(), [&](auto const idx) {
+    if (idx % mod == 0) {
+      uLOG(info) << desc << " " << idx << "/" << jobs.size();
+    }
+    func(jobs[idx]);
+  });
 }
 
 }  // namespace utl
