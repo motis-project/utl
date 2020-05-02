cc9d41e59055628cefa759aa82c0c64f858ca0c6
Felix Gündling <felix.guendling@gmail.com>
2020-05-02 19:45:11 +0200

    read_file function

---
+++ include/utl/read_file.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <fstream>
+#include <optional>
+
+namespace utl {
+
+inline std::optional<std::string> read_file(char const* path) {
+  try {
+    std::ifstream f{path, std::ios::binary | std::ios::ate};
+    f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    auto const size = f.tellg();
+    f.seekg(0, std::ios::beg);
+
+    std::string buffer;
+    buffer.resize(size);
+
+    return f.read(&buffer[0], size) ? std::make_optional(buffer) : std::nullopt;
+  } catch (std::exception const& e) {
+    return std::nullopt;
+  }
+}
+
+}  // namespace utl
