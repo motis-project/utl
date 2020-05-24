33e126b2d93a4fc0d8ee07b61ff34362df8bc87d
Felix Gündling <felix.guendling@gmail.com>
2020-05-24 15:07:43 +0200

    parallel_for: introduce error handling strategy
    quit strategy:
      - as before, quit after first exception but
      - rethrow first exception after join
    
    continue strategy:
      - continue after exception
      - collect all exceptions (std::exception_ptr) and
      - return them to the called

--- include/utl/parallel_for.h
+++ include/utl/parallel_for.h
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 #include <atomic>
+#include <exception>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -10,40 +12,73 @@
 
 namespace utl {
 
+enum class parallel_error_strategy { CONTINUE_EXEC, QUIT_EXEC };
+
+using errors_t = std::vector<std::pair<size_t, std::exception_ptr>>;
+
 template <typename Fun>
-inline void parallel_for_run(size_t const job_count, Fun func) {
+inline errors_t parallel_for_run(size_t const job_count, Fun func,
+                                 parallel_error_strategy const err_strat =
+                                     parallel_error_strategy::QUIT_EXEC) {
+  errors_t errors;
+  std::mutex errors_mutex;
   std::atomic<size_t> counter(0);
+  std::atomic<bool> quit{false};
   std::vector<std::thread> threads;
   for (auto i = 0u; i < std::thread::hardware_concurrency(); ++i) {
     threads.emplace_back([&]() {
-      while (true) {
+      while (!quit) {
         auto const idx = counter.fetch_add(1);
         if (idx >= job_count) {
           break;
         }
 
-        func(idx);
+        try {
+          func(idx);
+        } catch (...) {
+          std::lock_guard lock{errors_mutex};
+          errors.emplace_back(std::pair{i, std::current_exception()});
+          if (err_strat == parallel_error_strategy::QUIT_EXEC) {
+            quit = true;
+            break;
+          }
+        }
       }
     });
   }
 
   std::for_each(begin(threads), end(threads), [](auto& t) { t.join(); });
+
+  if (err_strat == parallel_error_strategy::QUIT_EXEC && !errors.empty()) {
+    std::rethrow_exception(errors.front().second);
+  }
+
+  return errors;
 }
 
 template <typename Container, typename Fun>
-inline void parallel_for(Container const& jobs, Fun&& func) {
-  parallel_for_run(jobs.size(), [&](auto const idx) { func(jobs[idx]); });
+inline errors_t parallel_for(Container const& jobs, Fun&& func,
+                             parallel_error_strategy const err_strat =
+                                 parallel_error_strategy::QUIT_EXEC) {
+  return parallel_for_run(
+      jobs.size(), [&](auto const idx) { func(jobs[idx]); }, err_strat);
 }
 
 template <typename T, typename Fun>
-inline void parallel_for(std::string const& desc, std::vector<T> const& jobs,
-                         size_t const mod, Fun func) {
-  parallel_for_run(jobs.size(), [&](auto const idx) {
-    if (idx % mod == 0) {
-      uLOG(info) << desc << " " << idx << "/" << jobs.size();
-    }
-    func(jobs[idx]);
-  });
+inline errors_t parallel_for(std::string const& desc,
+                             std::vector<T> const& jobs, size_t const mod,
+                             Fun func,
+                             parallel_error_strategy const err_strat =
+                                 parallel_error_strategy::QUIT_EXEC) {
+  return parallel_for_run(
+      jobs.size(),
+      [&](auto const idx) {
+        if (idx % mod == 0) {
+          uLOG(info) << desc << " " << idx << "/" << jobs.size();
+        }
+        func(jobs[idx]);
+      },
+      err_strat);
 }
 
 }  // namespace utl
