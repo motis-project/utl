f078884e55dfde1ec1817e266ee2e39a391e8634
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-06-20 11:47:00 +0200

    repeat_n: add Container template paramter for cista compatibility

--- include/utl/repeat_n.h
+++ include/utl/repeat_n.h
@@ -6,9 +6,9 @@
 namespace utl {
 
 // TODO replace this in motis::loader
-template <typename T>
-std::vector<T> repeat_n(T const& el, size_t const n) {
-  std::vector<T> els(n);
+template <typename T, typename Container = std::vector<T>>
+Container repeat_n(T const& el, size_t const n) {
+  Container els(n);
   std::fill(begin(els), end(els), el);
   return els;
 }
