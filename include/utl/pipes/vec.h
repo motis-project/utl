caf84cc5214c41dc702d199f69105de44f112b77
Felix Gündling <felix.guendling@gmail.com>
2020-07-08 13:46:57 +0200

    pipes: generic to<T> additional to vec()

--- include/utl/pipes/vec.h
+++ include/utl/pipes/vec.h
@@ -22,4 +22,23 @@ struct to_vec_t {
 
 inline to_vec_t vec() { return to_vec_t(); }
 
+template <typename Container>
+struct to_t {
+  template <typename T>
+  friend auto operator|(T&& r, to_t&&) {
+    auto it = r.begin();
+    Container c;
+    while (r.valid(it)) {
+      c.emplace(r.read(it));
+      r.next(it);
+    }
+    return c;
+  }
+};
+
+template <typename Container>
+inline to_t<Container> to() {
+  return {};
+}
+
 }  // namespace utl
