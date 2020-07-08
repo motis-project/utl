368833553467bd0259432e5fbea0d3d2863698d7
Felix Gündling <felix.guendling@gmail.com>
2020-07-08 13:44:22 +0200

    visit.h -> overloaded.h, allow leaving out types (catched by default impl)

---
+++ include/utl/overloaded.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace utl {
+
+template <typename Ret, typename Arg, typename... Rest>
+Arg first_argument_helper(Ret (*)(Arg, Rest...));
+
+template <typename Ret, typename F, typename Arg, typename... Rest>
+Arg first_argument_helper(Ret (F::*)(Arg, Rest...));
+
+template <typename Ret, typename F, typename Arg, typename... Rest>
+Arg first_argument_helper(Ret (F::*)(Arg, Rest...) const);
+
+template <typename F>
+decltype(first_argument_helper(&F::operator())) first_argument_helper(F);
+
+template <typename T>
+using first_argument = decltype(first_argument_helper(std::declval<T>()));
+
+template <class... Ts>
+struct overloaded : Ts... {
+  using Ts::operator()...;
+
+  template <typename T>
+  std::enable_if_t<
+      (!std::is_same_v<std::decay_t<T>, std::decay_t<first_argument<Ts>>> &&
+       ...)>
+  operator()(T&&) {}
+};
+
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+}  // namespace utl
