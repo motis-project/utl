3195a2f32b2bb48e924bdf7853789664ad844f1f
julianharbarth <64156615+julianharbarth@users.noreply.github.com>
2020-08-05 12:06:18 +0000

    erase_duplicates with iterators (#5)

--- include/utl/erase_duplicates.h
+++ include/utl/erase_duplicates.h
@@ -16,4 +16,11 @@ void erase_duplicates(Container& c, Less&& less, Eq&& eq) {
   c.erase(std::unique(begin(c), end(c), eq), end(c));
 }
 
+template <typename Container, typename Iterator, typename Less, typename Eq>
+void erase_duplicates(Container& c, Iterator const begin, Iterator const end,
+                      Less&& less, Eq&& eq) {
+  std::sort(begin, end, less);
+  c.erase(std::unique(begin, end, eq), end);
+}
+
 }  // namespace utl
