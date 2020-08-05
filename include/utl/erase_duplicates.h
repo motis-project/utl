fd4acd95614d3b416780773d4881670b179280d9
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2020-08-05 13:57:34 +0200

    please the formatter

--- include/utl/erase_duplicates.h
+++ include/utl/erase_duplicates.h
@@ -18,7 +18,7 @@ void erase_duplicates(Container& c, Less&& less, Eq&& eq) {
 
 template <typename Container, typename Iterator, typename Less, typename Eq>
 void erase_duplicates(Container& c, Iterator const begin, Iterator const end,
-                     Less&& less, Eq&& eq) {
+                      Less&& less, Eq&& eq) {
   std::sort(begin, end, less);
   c.erase(std::unique(begin, end, eq), end);
 }
