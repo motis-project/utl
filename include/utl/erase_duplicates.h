a7a25bb59324da3b00b7aab6da49a82dba28e9b4
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2020-08-05 13:39:07 +0200

    revert name change, remove newline

--- include/utl/erase_duplicates.h
+++ include/utl/erase_duplicates.h
@@ -5,20 +5,19 @@
 namespace utl {
 
 template <typename Container>
-void sort_and_unique(Container& c) {
+void erase_duplicates(Container& c) {
   std::sort(begin(c), end(c));
   c.erase(std::unique(begin(c), end(c)), end(c));
 }
 
 template <typename Container, typename Less, typename Eq>
-void sort_and_unique(Container& c, Less&& less, Eq&& eq) {
+void erase_duplicates(Container& c, Less&& less, Eq&& eq) {
   std::sort(begin(c), end(c), less);
   c.erase(std::unique(begin(c), end(c), eq), end(c));
 }
 
 template <typename Container, typename Iterator, typename Less, typename Eq>
-
-void sort_and_unique(Container& c, Iterator const& begin, Iterator const& end,
+void erase_duplicates(Container& c, Iterator const& begin, Iterator const& end,
                      Less&& less, Eq&& eq) {
   std::sort(begin, end, less);
   c.erase(std::unique(begin, end, eq), end);
