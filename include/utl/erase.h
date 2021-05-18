571abc1973838b36931c64b2c7d98cc47e34da34
Pablo Hoch <pablohoch@users.noreply.github.com>
2021-05-18 15:14:00 +0200

    Add erase(Container&&...) and erase_if(Container&&...) (#11)

--- include/utl/erase.h
+++ include/utl/erase.h
@@ -10,8 +10,18 @@ void erase(Container& c, Element elem) {
   c.erase(std::remove(begin(c), end(c), elem), end(c));
 }
 
+template <typename Container, typename Element>
+void erase(Container&& c, Element elem) {
+  c.erase(std::remove(begin(c), end(c), elem), end(c));
+}
+
 template <typename T, typename Element>
 void erase(std::set<T>& c, Element elem) {
+  c.erase(elem);
+}
+
+template <typename T, typename Element>
+void erase(std::set<T>&& c, Element elem) {
   c.erase(elem);
 }
 
--- include/utl/erase_if.h
+++ include/utl/erase_if.h
@@ -9,4 +9,9 @@ void erase_if(Container& c, Predicate pred) {
   c.erase(std::remove_if(begin(c), end(c), pred), end(c));
 }
 
+template <typename Container, typename Predicate>
+void erase_if(Container&& c, Predicate pred) {
+  c.erase(std::remove_if(begin(c), end(c), pred), end(c));
+}
+
 }  // namespace utl
