dd9b2b58c962467c32a181a7041faf60b3f155f4
Felix Gündling <felix.guendling@gmail.com>
2023-06-01 08:49:00 +0200

    insert_sorted (unique)

---
+++ include/utl/insert_sorted.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <algorithm>
+#include <utility>
+
+namespace utl {
+
+template <typename Collection, typename Less>
+std::pair<typename std::decay_t<Collection>::iterator, bool> insert_sorted(
+    Collection&& v, typename std::decay_t<Collection>::value_type el,
+    Less&& less) {
+  using std::begin;
+  using std::end;
+  auto const it =
+      std::lower_bound(begin(v), end(v), el, std::forward<Less>(less));
+  if (it == std::end(v) || *it != el) {
+    return {v.insert(it, std::move(el)), true};
+  }
+  return {it, false};
+}
+
+template <typename Collection>
+std::pair<typename std::decay_t<Collection>::iterator, bool> insert_sorted(
+    Collection&& v, typename Collection::value_type el) {
+  return insert_sorted(std::forward<Collection>(v), std::move(el),
+                       std::less<typename Collection::value_type>{});
+}
+
+}  // namespace utl
