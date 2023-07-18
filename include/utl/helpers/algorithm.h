b81cfdccda166cef3c20dd41b4c9697b4137cea4
Felix Gündling <felix.guendling@gmail.com>
2023-07-18 11:59:14 +0200

    utl::find

--- include/utl/helpers/algorithm.h
+++ include/utl/helpers/algorithm.h
@@ -33,6 +33,13 @@ constexpr auto find_if(Collection&& c, Predicate&& p) {
   return std::find_if(begin(c), end(c), std::forward<Predicate>(p));
 }
 
+template <typename Collection, typename Item>
+constexpr auto find(Collection&& c, Item&& p) {
+  using std::begin;
+  using std::end;
+  return std::find(begin(c), end(c), std::forward<Item>(p));
+}
+
 template <typename Collection, typename Predicate>
 auto count_if(Collection&& c, Predicate&& p) {
   using std::begin;
