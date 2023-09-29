324626e2804c5e324afaa8f7a8a5b93b8771060c
Felix Gündling <felix.guendling@gmail.com>
2023-09-29 12:41:18 +0200

    parallel_for_run_threadlocal

--- include/utl/parallel_for.h
+++ include/utl/parallel_for.h
@@ -20,6 +20,52 @@ struct noop_progress_update {
   void operator()() {}
 };
 
+template <typename ThreadLocal, typename Fun,
+          typename ProgressUpdateFn = noop_progress_update>
+inline errors_t parallel_for_run_threadlocal(
+    size_t const job_count, Fun func,
+    ProgressUpdateFn&& progress_update = ProgressUpdateFn{},
+    parallel_error_strategy const err_strat =
+        parallel_error_strategy::QUIT_EXEC) {
+  errors_t errors;
+  std::mutex errors_mutex;
+  std::atomic<size_t> counter(0);
+  std::atomic<bool> quit{false};
+  std::vector<std::thread> threads;
+  for (auto i = 0u; i < std::thread::hardware_concurrency(); ++i) {
+    threads.emplace_back([&]() {
+      auto threadlocal = ThreadLocal{};
+
+      while (!quit) {
+        auto const idx = counter.fetch_add(1);
+        if (idx >= job_count) {
+          break;
+        }
+
+        try {
+          func(threadlocal, idx);
+          progress_update();
+        } catch (...) {
+          std::lock_guard<std::mutex> lock{errors_mutex};
+          errors.emplace_back(std::pair{i, std::current_exception()});
+          if (err_strat == parallel_error_strategy::QUIT_EXEC) {
+            quit = true;
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  std::for_each(begin(threads), end(threads), [](auto& t) { t.join(); });
+
+  if (err_strat == parallel_error_strategy::QUIT_EXEC && !errors.empty()) {
+    std::rethrow_exception(errors.front().second);
+  }
+
+  return errors;
+}
+
 template <typename Fun, typename ProgressUpdateFn = noop_progress_update>
 inline errors_t parallel_for_run(
     size_t const job_count, Fun func,
