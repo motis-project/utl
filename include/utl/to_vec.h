77aac494c45d2b070e65fe712abc34ac74a91d0f
Pablo Hoch <pablo@melaxis.com>
2024-08-15 13:08:21 +0200

    revert to_vec changes for now
    this broke a lot of code in motis

--- include/utl/to_vec.h
+++ include/utl/to_vec.h
@@ -8,16 +8,18 @@ namespace utl {
 
 template <typename Output, typename Container, typename UnaryOperation>
 inline void transform_to(Container&& c, Output& out, UnaryOperation&& op) {
-  out.reserve(static_cast<std::size_t>(std::distance(begin(c), end(c))));
-  std::transform(begin(c), end(c), std::back_inserter(out),
+  out.reserve(
+      static_cast<std::size_t>(std::distance(std::begin(c), std::end(c))));
+  std::transform(std::begin(c), std::end(c), std::back_inserter(out),
                  std::forward<UnaryOperation>(op));
 }
 
 template <typename Output, typename Container, typename UnaryOperation>
 inline auto transform_to(Container&& c, UnaryOperation&& op) -> Output {
   Output v;
-  v.reserve(static_cast<std::size_t>(std::distance(begin(c), end(c))));
-  std::transform(begin(c), end(c), std::back_inserter(v),
+  v.reserve(
+      static_cast<std::size_t>(std::distance(std::begin(c), std::end(c))));
+  std::transform(std::begin(c), std::end(c), std::back_inserter(v),
                  std::forward<UnaryOperation>(op));
   return v;
 }
@@ -33,24 +35,22 @@ inline auto to_vec(It s, It e, UnaryOperation&& op)
 
 template <typename Container, typename UnaryOperation>
 inline auto to_vec(Container&& c, UnaryOperation&& op)
-    -> std::vector<std::decay_t<decltype(op(*begin(c)))>> {
-  using std::begin;
-  using std::end;
-  auto v = std::vector<std::decay_t<decltype(op(*begin(c)))>>{};
-  v.reserve(c.size());
-  std::transform(begin(c), end(c), std::back_inserter(v),
+    -> std::vector<std::decay_t<decltype(op(*std::begin(c)))>> {
+  auto v = std::vector<std::decay_t<decltype(op(*std::begin(c)))>>{};
+  v.reserve(
+      static_cast<std::size_t>(std::distance(std::begin(c), std::end(c))));
+  std::transform(std::begin(c), std::end(c), std::back_inserter(v),
                  std::forward<UnaryOperation>(op));
   return v;
 }
 
 template <typename Container>
 inline auto to_vec(Container&& c)
-    -> std::vector<std::decay_t<decltype(*begin(c))>> {
-  using std::begin;
-  using std::end;
-  auto v = std::vector<std::decay_t<decltype(*begin(c))>>{};
-  v.reserve(c.size());
-  std::copy(begin(c), end(c), std::back_inserter(v));
+    -> std::vector<std::decay_t<decltype(*std::begin(c))>> {
+  auto v = std::vector<std::decay_t<decltype(*std::begin(c))>>{};
+  v.reserve(
+      static_cast<std::size_t>(std::distance(std::begin(c), std::end(c))));
+  std::copy(std::begin(c), std::end(c), std::back_inserter(v));
   return v;
 }
 
