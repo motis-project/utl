fd1f5a2a79a373d7c9cf9f3b18db8e3e22aa22a6
Traines <git@traines.eu>
2025-05-12 12:31:24 +0000

    fix split test

--- test/parser/split_test.cc
+++ test/parser/split_test.cc
@@ -1,18 +1,21 @@
-#include "utl/parser/split.h"
+#include <tuple>
+
 #include "gtest/gtest.h"
+#include "utl/parser/cstr.h"
+#include "utl/parser/split.h"
 
 using namespace utl;
 
 TEST(split, simple_split) {
   auto const chunks = utl::split<'_', utl::cstr, utl::cstr, utl::cstr>("a_b_c");
-  EXPECT_EQ(cstr{"a"}, std::get<0>(chunks));
-  EXPECT_EQ(cstr{"b"}, std::get<1>(chunks));
-  EXPECT_EQ(cstr{"c"}, std::get<2>(chunks));
+  EXPECT_EQ(utl::cstr{"a"}, std::get<0>(chunks));
+  EXPECT_EQ(utl::cstr{"b"}, std::get<1>(chunks));
+  EXPECT_EQ(utl::cstr{"c"}, std::get<2>(chunks));
 }
 
 TEST(split, too_few_split) {
   auto const chunks = utl::split<'_', utl::cstr, utl::cstr, utl::cstr>("a_b");
-  EXPECT_EQ(cstr{"a"}, std::get<0>(chunks));
-  EXPECT_EQ(cstr{"b"}, std::get<1>(chunks));
-  EXPECT_EQ(cstr{"\0"}, std::get<2>(chunks));
+  EXPECT_EQ(utl::cstr{"a"}, std::get<0>(chunks));
+  EXPECT_EQ(utl::cstr{"b"}, std::get<1>(chunks));
+  EXPECT_EQ(utl::cstr{"\0"}, std::get<2>(chunks));
 }
