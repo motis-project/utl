b2736ac3bd43117cba4ddcedaed866fbb6850908
Traines <git@traines.eu>
2025-05-13 10:30:28 +0000

    split test starting_underscore

--- test/parser/split_test.cc
+++ test/parser/split_test.cc
@@ -17,5 +17,15 @@ TEST(split, too_few_split) {
   auto const chunks = utl::split<'_', utl::cstr, utl::cstr, utl::cstr>("a_b");
   EXPECT_EQ(utl::cstr{"a"}, std::get<0>(chunks));
   EXPECT_EQ(utl::cstr{"b"}, std::get<1>(chunks));
-  EXPECT_EQ(utl::cstr{"\0"}, std::get<2>(chunks));
+  EXPECT_EQ(utl::cstr{""}, std::get<2>(chunks));
+  EXPECT_FALSE(std::get<2>(chunks).valid());
+}
+
+TEST(split, starting_underscore) {
+  auto const chunks =
+      utl::split<'_', utl::cstr, utl::cstr, utl::cstr>("a_b__c");
+  EXPECT_EQ(utl::cstr{"a"}, std::get<0>(chunks));
+  EXPECT_EQ(utl::cstr{"b"}, std::get<1>(chunks));
+  EXPECT_EQ(utl::cstr{""}, std::get<2>(chunks));
+  EXPECT_FALSE(std::get<2>(chunks).valid());
 }
