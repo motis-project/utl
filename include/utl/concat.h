3d4d1f98aaf8532869361d901e010dac11763230
Felix Gündling <felix.guendling@gmail.com>
2025-12-11 15:11:20 +0100

    add merge to concat.h if you want a new copy with both merged

--- include/utl/concat.h
+++ include/utl/concat.h
@@ -10,4 +10,14 @@ Container1& concat(Container1& c1, Container2 const& c2) {
   return c1;
 }
 
+template <typename Container1, typename Container2>
+Container1 merge(Container1 const& c1, Container2 const& c2) {
+  if (c2.empty()) {
+    return c1;
+  }
+  auto c = c1;
+  c.insert(end(c), begin(c2), end(c2));
+  return c;
+}
+
 }  // namespace utl
